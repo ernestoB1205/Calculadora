035d2eb3c9969eaa49d3bbae6e84029b4980a131
Bravo Huaman Ernesto <ernesto12raulito@gmail.com>
2025-04-25 15:20:44 -0500

    feat: add class calculadora y comentarios

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -4,17 +4,18 @@
  */
 package calculadora;
 
-import javax.swing.*;              
-import java.awt.*;                
-import java.awt.event.*;          
-import java.util.ArrayList;       
+import javax.swing.*;     // Librería para componentes gráficos (ventanas, botones, etc.)         
+import java.awt.*;          //Librería para layouts y colores
+import java.awt.event.*;     //Librería para manejo de eventos (clics, acciones)     
+import java.util.ArrayList;    // Colección para guardar los resultados.   
 
 public class Calculadora extends JFrame {
-    
+
+    // Campos de texto para ingresar números y mostrar el resultado
     private JTextField campo1, campo2, campoResultado;
-
+    // Botones para realizar operaciones
     private JButton botonSumar, botonDividir, botonMostrar;
-
+    // Lista para almacenar los resultados de las operaciones
     private ArrayList<Double> resultados;
 
     public static void main(String[] args) {
